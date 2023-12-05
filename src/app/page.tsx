9ca2ba6975399b835acd03056f853c97d082b6ad
1red0 <andumarius9@gmail.com>
2023-12-05 21:48:01 +0200

    add header main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,31 +74,40 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-screen flex flex-col md:flex-row bg-base-300 overflow-x-auto">
-      {/* Presentation Container */}
-      <div className="md:flex-1 flex justify-center p-8 bg-base-300 min-w-screen-md mx-auto md:max-w-screen-2xl overflow-x-auto">
-        <div className="max-w-lg text-center">
-          <div className="mockup-window border bg-base-200 shadow-xl">
-            <div className="flex justify-center px-4 md:px-0 py-16 bg-base-100">
-              <div className="hero min-w-screen bg-base-100">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                  <Image src={Logo} alt='LOGO' width={200} height={234} />
-                  <div>
-                    <h1 className="text-5xl font-bold">ACE</h1>
-                    <p className="py-6">Facultatea de Automatică, Calculatoare și Electronică</p>
+    <><header className="text-center py-20 bg-base-200 border md:flex-row overflow-x-auto min-w-screen-md" >
+      <h1 className="text-2xl font-bold mb-4 drop-shadow-2xl">
+        <span>Universitatea din Craiova</span>
+      </h1>
+      <h1 className="text-xl font-bold mb-4 drop-shadow-2xl">
+        <span>Facultatea de Automatică, Calculatoare și Electronică</span>
+      </h1>
+    </header>
+    <div className="min-h-screen flex flex-col md:flex-row bg-base-300 overflow-x-auto border">
+        {/* Presentation Container */}
+        <div className="md:flex-1 flex justify-center p-8 bg-base-300 min-w-screen-md mx-auto md:max-w-screen-2xl overflow-x-auto">
+          <div className="max-w-lg text-center">
+            <div className="mockup-window border bg-base-200 shadow-xl">
+              <div className="flex justify-center px-4 md:px-0 py-16 bg-base-100">
+                <div className="hero min-w-screen bg-base-100">
+                  <div className="hero-content flex-col lg:flex-row-reverse">
+                    <Image src={Logo} alt='LOGO' width={200} height={234} />
+                    <div className='p-16'>
+                      <h1 className="text-5xl font-bold">ACE</h1>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      {/* News Container */}
-      <div className="md:flex-1 flex flex-col max-w-screen-md mx-auto items-center justify-center p-8 bg-base-100 overflow-x-auto border md:max-w-screen-2xl">
-        <Link href="/news"><button className="btn btn-primary mb-4">Toate știrile</button></Link>
-        {newsContent}
-      </div>
-    </div>
+
+
+        {/* News Container */}
+        <div className="md:flex-1 flex flex-col max-w-screen-md mx-auto items-center justify-center p-8 bg-base-100 overflow-x-auto border md:max-w-screen-2xl">
+          <Link href="/news"><button className="btn btn-primary mb-4">Toate știrile</button></Link>
+          {newsContent}
+        </div>
+      </div></>
   );
 }
