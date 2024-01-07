cd1c092d411e8c4f221b048b781d207f90e8b79d
Andrei <andrei.rosu@empact.app>
2024-01-07 21:26:31 +0200

    add missing details

--- README.md
+++ README.md
@@ -18,6 +18,16 @@ npm run dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+You will need to have the following environment files on your machine for the connection to the database:
+./.env.development.local
+./prisma/.env
+
+### Browse prisma data
+
+```bash
+npx prisma studio
+```
+
 ## Deploy on Vercel
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
--- src/app/api/post/route.tsx
+++ src/app/api/post/route.tsx
@@ -1,10 +1,15 @@
 import prisma from "../../../../lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
+/**
+ * Handles a GET request to retrieve news details from a database using Prisma.
+ * @param req - The request object containing information about the incoming request.
+ * @returns A JSON response with the news details, a message and a 404 status code if no news details are found, or an error message and a 500 status code if an error occurs.
+ */
+
 export async function GET(req: NextRequest) {
 
     const id = String(req.nextUrl.searchParams.get('id'))
-    console.log(id)
 
     try {
         const newsDetails = await prisma.post.findUnique({
@@ -21,6 +26,5 @@ export async function GET(req: NextRequest) {
         console.error(error);
         return NextResponse.json({ error: 'An error occurred while fetching news details' }, { status: 500 });
     }
-  
 
 }
